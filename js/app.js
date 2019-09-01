a2bf54b370986d47638cdaf3b3992d64ac24695e
Ahmad Shadeed <shadeed9@gmail.com>
2019-09-01 20:58:46 +0300

    more js progress

--- js/app.js
+++ js/app.js
@@ -1,22 +1,71 @@
-//var cols = document.querySelector('#columns');
 var minCol = document.querySelector('#itemWidth');
+var gridColGap = document.querySelector('#gridColGap');
+var gridRowGap = document.querySelector('#gridRowGap');
+var unifyGap = document.querySelector('#unifyGap');
 var gridWrapper = document.querySelector('.js-grid');
-//var numOfCols = cols.value;
-var minColWidth = minCol.value;
+
+var minColWidth = 200;
+var gridColGapValue = 16;
+var gridRowGapValue = 16;
+
+var isUnify = false;
+
+// Asign default value for inputs
+minCol.value = minColWidth;
+gridColGap.value = gridColGapValue;
+gridRowGap.value = gridRowGapValue;
 
 minCol.addEventListener('input', function () {
+    if(this.value < 0) {
+        return;
+    }
     minColWidth = this.value;
     generateGridItems();
 });
 
-// cols.addEventListener('input', function () {
-//     numOfCols = this.value;
-//     generateGridItems();
-// });
+gridColGap.addEventListener('input', function () {
+    gridColGapValue = this.value;
+    generateGridItems();
+});
+
+gridRowGap.addEventListener('input', function () {
+    gridRowGapValue = this.value;
+    generateGridItems();
+});
+
+unifyGap.addEventListener('input', function () {
+   if (this.checked) {
+       gridColGapValue = 16;
+       gridRowGapValue = 16;
+
+       gridColGap.value = gridColGapValue;
+       gridRowGap.value = gridRowGapValue;
+
+       isUnify = true;
+
+       generateGridItems();
+   }
+});
+
+generateGridItems();
 
 function generateGridItems() {
-    gridWrapper.style.gridTemplateColumns = `repeat(auto-fill, minmax(${minColWidth}px, 1fr))`;
-    console.log('Hello');
-}
+    if(isUnify) {
+        // Get the current focused element
+        var currentActiveElem = document.activeElement;
 
-//console.log(numOfCols);
+        if(currentActiveElem.getAttribute('id') == 'gridColGap') {
+            gridRowGapValue = gridColGapValue;
+        }
+
+        if(currentActiveElem.getAttribute('id') == 'gridRowGap') {
+            gridColGapValue = gridRowGapValue;
+        }
+
+        gridColGap.value = gridColGapValue;
+        gridRowGap.value = gridRowGapValue;
+    }
+    gridWrapper.style.gridTemplateColumns = `repeat(auto-fit, minmax(${minColWidth}px, 1fr))`;
+    gridWrapper.style.gridColumnGap = `${gridColGapValue}px`;
+    gridWrapper.style.gridRowGap = `${gridRowGapValue}px`;
+}
